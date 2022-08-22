9f10fcf5465851fed09968e570e375ae3ec7b865
Dranoel224 <ringebong@gmail.com>
2022-08-22 12:14:33 +0100

    Last updated

--- counter/counterFunction.js
+++ counter/counterFunction.js
@@ -4,20 +4,19 @@ function timeSetter(){
     countTime = setInterval(countDownClock, 10000); 
 }
 
-let countDownClock = document.getElementsById("container").innerHTML = function () {
+let countDownClock =  function () {
     //displayUI = document.getElementsById("container");
     for (let counter = 60; counter >= 0; counter -= 5) {
         if (counter % 2 == 0) {
-            document.getElementsById("container").style.background = "yellow";
+            document.getElementsById("container").style.backgroundColor = "yellow";
         }else if (counter % 5 == 0) {
-            document.getElementsById("container").style.background = "green";
+            document.getElementsById("container").style.backgroundColor = "green";
         } else if (counter < 10) {
-            document.getElementsById("container").style.background = "red";
+            document.getElementsById("container").style.backgroundColor = "red";
         }
         return counter;
         
     }
-    
     console.log(countDownClock());
     document.getElementsById("container").innerHTML += countDownClock;
 }
